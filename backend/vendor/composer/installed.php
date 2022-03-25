d954ca74cfba4d1186b3981a0fe000cfa78eaf3d
Nick Maietta <nick@premoweb.com>
2022-03-25 08:42:44 -0700

    Ran composer install

---
+++ backend/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../../',
+        'aliases' => array(),
+        'reference' => 'e3aa100b714dad40a08938fc1eab4c09da28e43c',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../../',
+            'aliases' => array(),
+            'reference' => 'e3aa100b714dad40a08938fc1eab4c09da28e43c',
+            'dev_requirement' => false,
+        ),
+        'bcosca/fatfree-core' => array(
+            'pretty_version' => '3.8.0',
+            'version' => '3.8.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../bcosca/fatfree-core',
+            'aliases' => array(),
+            'reference' => 'cdd878db018280369b643fce62f70e55044f9ac4',
+            'dev_requirement' => false,
+        ),
+    ),
+);
