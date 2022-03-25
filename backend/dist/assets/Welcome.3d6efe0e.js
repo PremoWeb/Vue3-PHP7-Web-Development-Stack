a6c5d1a76e43b3bf873a80a282cdb027012a8b38
Nick Maietta <nick@premoweb.com>
2022-03-25 15:44:26 -0700

    Test

---
+++ backend/dist/assets/Welcome.3d6efe0e.js
@@ -0,0 +1,4 @@
+/**
+ * Built by PremoWeb (https://premoweb.com/).
+ */
+import{_ as e}from"./index.f363025b.js";import{w as r,x as c}from"./vendor.bfe51a30.js";const o={};function t(n,a){return r(),c("p",null,"Welcome!")}var m=e(o,[["render",t]]);export{m as default};
